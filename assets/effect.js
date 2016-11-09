734d278e6c7e66b10a78c2574edf011bdc281a26
Carlos Gonzalez <cubeton@gmail.com>
2016-11-08 21:56:50 -0500

    adding more content

--- assets/effect.js
+++ assets/effect.js
@@ -1,12 +1,74 @@
+var time = moment().format('h:mm:ss A');
+var timeOfDay = parseInt(moment().format('H'));
+
+console.log(time);
+console.log(timeOfDay);
+
+//if(moment().isBefore('12:00:00')){
+//  console.log('hey');
+//}
+
 
 $(document).ready(function () {
 
-    $('.type-writer').typeIt({
-        strings:'Love Web Development !',
-        speed: 50,
-        autoStart:true
+  $('.type-writer2').typeIt({
+    strings: '<strong>Hola !</strong>',
+    speed: 50,
+    autoStart: true
 
-    });
+  });
 
-})
 
+  var $timeDisplay = $('<div/>').addClass('time').html(time);
+  $('.top-banner').html($timeDisplay);
+
+  
+  
+//   if (timeOfDay >= 19 && timeOfDay < 6) {
+//    $('.top-content').css('background-image', 'url(' + "./assets/img/morning.jpg" + ')');
+//
+//  }
+
+  if (timeOfDay >= 6 && timeOfDay < 12) {
+    $('.top-content').css('background-image', 'url(' + "./assets/img/morning.jpg" + ')');
+
+  }
+
+  if (timeOfDay >= 12 && timeOfDay < 17) {
+    $('.top-content').css('background-image', 'url(' + "./assets/img/day.jpg" + ')');
+
+  }
+
+  if (timeOfDay >= 17 && timeOfDay < 19) {
+    $('.top-content').css('background-image', 'url(' + "./assets/img/dusk.jpg" + ')');
+
+  }
+  
+  if (timeOfDay >= 19) {
+    console.log('sds');
+    $('.top-content').css('background-image', 'url(' + "./assets/img/night.jpg" + ')');
+
+  }
+
+});
+
+$(document).ready(function () {
+
+  setInterval(function () {
+
+    var time = moment().format("h:mm:ss A");
+
+    var $timeDisplay = $('<div/>').addClass('time').html(time);
+    $('.top-banner').html($timeDisplay);
+  }, 1000)
+
+});
+
+
+
+
+
+//  var $timeDisplay = $('<div/>').addClass('time').html(time);
+//  $('.top-banner').append($timeDisplay);
+
+//})
